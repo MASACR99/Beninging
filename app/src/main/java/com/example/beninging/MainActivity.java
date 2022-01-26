4e9599c61194af702ee8606a7e90c8829bfe6c66
MASACR99 <joangil99@gmail.com>
2022-01-26 17:58:29 +0100

    Stars

--- app/src/main/java/com/example/beninging/MainActivity.java
+++ app/src/main/java/com/example/beninging/MainActivity.java
@@ -1,16 +1,16 @@
 package com.example.beninging;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.app.ListActivity;
 import android.view.View;
-import android.widget.AdapterView;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import java.util.ArrayList;
 
 public class MainActivity extends ListActivity {
-    private TextView selection;
     private static final String[] items={"lorem", "ipsum", "dolor",
             "sit", "amet",
             "consectetuer", "adipiscing", "elit", "morbi", "vel",
@@ -21,15 +21,77 @@ public class MainActivity extends ListActivity {
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        setContentView(R.layout.activity_main);
-        setListAdapter(new ArrayAdapter<String>(this,
-                R.layout.row, R.id.label,
-                items));
-        selection=(TextView)findViewById(R.id.selection);
+
+        ArrayList<RowModel> list=new ArrayList<RowModel>();
+
+        for (String s : items) {
+            list.add(new RowModel(s));
+        }
+
+        setListAdapter(new RatingAdapter(list));
     }
 
-    public void onListItemClick(ListView parent, View v,
-                                int position,	long id) {
-        selection.setText(items[position]);
+    private RowModel getModel(int position) {
+        return(((RatingAdapter)getListAdapter()).getItem(position));
+    }
+
+    class RatingAdapter extends ArrayAdapter<RowModel> {
+        RatingAdapter(ArrayList<RowModel> list) {
+            super(MainActivity.this, R.layout.row, R.id.label, list);
+        }
+
+        public View getView(int position, View convertView,
+                            ViewGroup parent) {
+            View row=super.getView(position, convertView, parent);
+            ViewHolder holder=(ViewHolder)row.getTag();
+
+            if (holder==null) {
+                holder=new ViewHolder(row);
+                row.setTag(holder);
+
+                RatingBar.OnRatingBarChangeListener l=
+                        new RatingBar.OnRatingBarChangeListener() {
+                            public void onRatingChanged(RatingBar ratingBar,
+                                                        float rating,
+                                                        boolean fromTouch)	{
+                                Integer myPosition=(Integer)ratingBar.getTag();
+                                RowModel model=getModel(myPosition);
+
+                                model.rating=rating;
+
+                                LinearLayout parent=(LinearLayout)ratingBar.getParent();
+                                TextView label=(TextView)parent.findViewById(R.id.label);
+
+                                label.setText(model.toString());
+                            }
+                        };
+
+                holder.rate.setOnRatingBarChangeListener(l);
+            }
+
+            RowModel model=getModel(position);
+
+            holder.rate.setTag(new Integer(position));
+            holder.rate.setRating(model.rating);
+
+            return(row);
+        }
+    }
+
+    class RowModel {
+        String label;
+        float rating=2.0f;
+
+        RowModel(String label) {
+            this.label=label;
+        }
+
+        public String toString() {
+            if (rating>=3.0) {
+                return(label.toUpperCase());
+            }
+
+            return(label);
+        }
     }
 }
