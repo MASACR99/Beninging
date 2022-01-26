833a75fb3ba76cfccbbfe922e0af22a308dae426
MASACR99 <joangil99@gmail.com>
2022-01-26 17:50:14 +0100

    Static

--- app/src/main/java/com/example/beninging/MainActivity.java
+++ app/src/main/java/com/example/beninging/MainActivity.java
@@ -1,30 +1,16 @@
 package com.example.beninging;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ListActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.app.ListActivity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.LayoutInflater;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends ListActivity {
-    TextView selection;
+    private TextView selection;
     private static final String[] items={"lorem", "ipsum", "dolor",
             "sit", "amet",
             "consectetuer", "adipiscing", "elit", "morbi", "vel",
@@ -36,33 +22,14 @@ public class MainActivity extends ListActivity {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.activity_main);
-        setListAdapter(new IconicAdapter());
+        setListAdapter(new ArrayAdapter<String>(this,
+                R.layout.row, R.id.label,
+                items));
         selection=(TextView)findViewById(R.id.selection);
     }
 
     public void onListItemClick(ListView parent, View v,
-                                int position, long id) {
+                                int position,	long id) {
         selection.setText(items[position]);
-    }
-
-    class IconicAdapter extends ArrayAdapter<String> {
-        IconicAdapter() {
-            super(MainActivity.this, R.layout.row, R.id.label, items);
-        }
-
-        public View getView(int position, View convertView,
-                            ViewGroup parent) {
-            View row=super.getView(position, convertView, parent);
-            ImageView icon=(ImageView)row.findViewById(R.id.icon);
-
-            if (items[position].length()>4) {
-                icon.setImageResource(R.drawable.no);
-            }
-            else {
-                icon.setImageResource(R.drawable.ok);
-            }
-
-            return(row);
-        }
     }
 }
