c570c1b4b6035091436712be2f4badc2c05e5e52
MASACR99 <joangil99@gmail.com>
2022-01-28 18:07:54 +0100

    Stars + RecyclerView

--- app/src/main/java/com/example/beninging/MainActivity.java
+++ app/src/main/java/com/example/beninging/MainActivity.java
@@ -8,90 +8,41 @@ import android.widget.ArrayAdapter;
 import android.widget.RatingBar;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.util.ArrayList;
 
-public class MainActivity extends ListActivity {
-    private static final String[] items={"lorem", "ipsum", "dolor",
-            "sit", "amet",
-            "consectetuer", "adipiscing", "elit", "morbi", "vel",
-            "ligula", "vitae", "arcu", "aliquet", "mollis",
-            "etiam", "vel", "erat", "placerat", "ante",
-            "porttitor", "sodales", "pellentesque", "augue", "purus"};
+
+public class MainActivity extends AppCompatActivity implements MyRecyclerViewAdapter.ItemClickListener {
+    MyRecyclerViewAdapter adapter;
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        ArrayList<RowModel> list=new ArrayList<RowModel>();
+        // data to populate the RecyclerView with
+        ArrayList<String> animalNames = new ArrayList<>();
+        animalNames.add("Horse");
+        animalNames.add("Cow");
+        animalNames.add("Camel");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
 
-        for (String s : items) {
-            list.add(new RowModel(s));
-        }
-
-        setListAdapter(new RatingAdapter(list));
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyRecyclerViewAdapter(this, animalNames);
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
     }
 
-    private RowModel getModel(int position) {
-        return(((RatingAdapter)getListAdapter()).getItem(position));
-    }
-
-    class RatingAdapter extends ArrayAdapter<RowModel> {
-        RatingAdapter(ArrayList<RowModel> list) {
-            super(MainActivity.this, R.layout.row, R.id.label, list);
-        }
-
-        public View getView(int position, View convertView,
-                            ViewGroup parent) {
-            View row=super.getView(position, convertView, parent);
-            ViewHolder holder=(ViewHolder)row.getTag();
-
-            if (holder==null) {
-                holder=new ViewHolder(row);
-                row.setTag(holder);
-
-                RatingBar.OnRatingBarChangeListener l=
-                        new RatingBar.OnRatingBarChangeListener() {
-                            public void onRatingChanged(RatingBar ratingBar,
-                                                        float rating,
-                                                        boolean fromTouch)	{
-                                Integer myPosition=(Integer)ratingBar.getTag();
-                                RowModel model=getModel(myPosition);
-
-                                model.rating=rating;
-
-                                LinearLayout parent=(LinearLayout)ratingBar.getParent();
-                                TextView label=(TextView)parent.findViewById(R.id.label);
-
-                                label.setText(model.toString());
-                            }
-                        };
-
-                holder.rate.setOnRatingBarChangeListener(l);
-            }
-
-            RowModel model=getModel(position);
-
-            holder.rate.setTag(new Integer(position));
-            holder.rate.setRating(model.rating);
-
-            return(row);
-        }
-    }
-
-    class RowModel {
-        String label;
-        float rating=2.0f;
-
-        RowModel(String label) {
-            this.label=label;
-        }
-
-        public String toString() {
-            if (rating>=3.0) {
-                return(label.toUpperCase());
-            }
-
-            return(label);
-        }
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_SHORT).show();
     }
 }
